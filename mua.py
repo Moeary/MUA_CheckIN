d55316dee58894e0785394b67b08d7c459cb0347
Moeary <Minecraftyyxx@gmail.com>
2024-10-15 11:18:21 +0800

    Add wait time

--- mua.py
+++ mua.py
@@ -2,6 +2,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 import sys
 import os
@@ -37,18 +38,20 @@ try:
     time.sleep(5)
 
     # 使用显式等待查找签到按钮并检查是否可点击
-    wait = WebDriverWait(driver, 20)
-    sign_in_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.bg-gradient-primary.pl-4.pr-4")))
+    wait = WebDriverWait(driver, 10)  # 设置等待时间为10秒
+    try:
+        sign_in_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.bg-gradient-primary.pl-4.pr-4")))
 
-    # 检查按钮是否可点击
-    if sign_in_button.is_enabled():
-        sign_in_button.click()
-        print("签到按钮已点击")
-    else:
-        print("签到按钮不可点击")
-
-    # 等待签到完成
-    time.sleep(5)
+        # 检查按钮是否可点击
+        if sign_in_button.is_enabled():
+            sign_in_button.click()
+            print("签到按钮已点击")
+        else:
+            print("签到按钮不可点击")
+    except TimeoutException:
+        print("已经签到或按钮不可用")
+    finally:
+        driver.quit()
 
 finally:
     # 关闭浏览器并忽略OSError异常
